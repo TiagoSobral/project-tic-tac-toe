3a1d9faebcf78c9642158888a62f0584f9c0f0a0
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-06-03 14:28:27 +0100

    (feat) create console gameboard table

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+
+    for (let r = 0; r < rows; r++) {
+        board[r] = [];
+        for (let c = 0; c < columns; c++ ) {
+            board[r].push("");
+        }
+    }
+
+
+    return {board};
+}
