94322ed552df383bf057ffcf77d03150958dd66f
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-06-05 21:46:48 +0100

    (feat) add game flow function with play round func     -> need to print initial board when round starts     -> need to create a new turn with new player

--- javascript/script.js
+++ javascript/script.js
@@ -18,7 +18,7 @@ function gameBoard() {
         const cellValid = board.filter((element, index) => index === row).map((element) => element[column]);
 
         if (cellValid == ".") {
-            board[row][column] = mark;
+            return board[row][column] = mark;
         }
         else {
             console.log("Cell not available")
@@ -27,22 +27,14 @@ function gameBoard() {
     };
 
     const printBoard = () => {
-        return board;
+        console.log(board);
     };
 
     return {getBoard, chooseCell, printBoard};
 };
 
-const game = gameBoard();
 
-
-
-
-
-
-
-
-const createPlayer = (nameOne = "PlayerOne", nameTwo = "PlayerTwo") => {
+const createPlayers = (nameOne = "PlayerOne", nameTwo = "PlayerTwo") => {
     const players = [
         {   playerOneName: nameOne,
             mark: "X",
@@ -55,40 +47,27 @@ const createPlayer = (nameOne = "PlayerOne", nameTwo = "PlayerTwo") => {
 }
 
 
+function gameFlow() {
+    const board = gameBoard();
+
+    let playerOne = createPlayers().players[0];
+    let playerTwo = createPlayers().players[1];
+
+    const playRound = (row, column) => {
+        console.log(board.getBoard());
+        
+        // playerOne Turn
+        console.log(`Let's play, its your turn ...${playerOne.playerOneName}`);
+        board.chooseCell(row, column, playerOne.mark);
+        board.printBoard();
+
+    };
 
 
-// function createPlayer(name, mark) {
-//     // variables
-//     const players = {
-//         playerOne: name.at(0).toUpperCase() + name.slice(1),
-//         playerOneSymbol: mark.toUpperCase(),
-//         playerTwo: "Computer",
-//         playerTwoSymbol: function() { 
-//             if (mark === "X") {
-//             return playerTwoSymbol = "O";
-//             }
-//         else {
-//             return playerTwoSymbol = "X";
-//             }
-//         }    
-//     }
+    return {playRound};
+
+}
+
+const controller = gameFlow();
 
 
-//     opponentMark();
-
-//     return {playerOne, playerOneSymbol, playerTwo, playerTwoSymbol};
-// };
-
-
-
-
-// 1 - gameboard needs to show on console
-// 2 - player chooses name and mark & computer gets given a mark based on player choice
-// 3 - play round
-//  3.1 - user makes a play and chooses from the array where to place the mark
-//  3.2 - once chosen the mark is spliced to the place where the player chose
-//  3.3 - computer chooses where to place his mark based on where there is space(might want to cleverly place without just using random)
-//  3.4 - gameboard is updated with players choices
-// 4 - play rounds until one of them wins or draws
-// 5 - based on the gameboard status we check who wins 
-// 6 - gameboard is reset 
